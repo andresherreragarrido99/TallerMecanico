f15c621bdbd80a344b15b25f025f42e904df4f2e
nombre del usuario 2 <usuario2@example.com>
2026-04-22 10:38:58 +0200

    Enum fabricafuentedatos y clase fuentedatosmemoria

---
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/negocio/memoria/FuenteDatosMemoria.java
@@ -0,0 +1,12 @@
+package org.iesalandalus.programacion.tallermecanico.modelo.negocio.memoria;
+
+import org.iesalandalus.programacion.tallermecanico.modelo.negocio.*;
+
+public class FuenteDatosMemoria implements IFuenteDatos {
+    @Override
+    public IClientes crearClientes() { return new Clientes(); }
+    @Override
+    public IVehiculos crearVehiculos() { return new Vehiculos(); }
+    @Override
+    public ITrabajos crearTrabajos() { return new Trabajos(); }
+}
